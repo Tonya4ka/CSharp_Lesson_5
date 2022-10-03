1e411fb55b95d1b5657cdb947c48cdcf126a8b17
tonyatrushkova <antonina.trushkova@gmail.com>
2022-10-03 14:02:36 +0300

    Add Exercise_36

---
+++ Exercise_36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.Write("Type size of the array: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"The array has {array.Length} numbers, the sum of elements located at odd indices is {SumOddPositionNumbers(array)}");
+
+void FillArray(int[] array)
+{
+    Random RandGen = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = RandGen.Next(1,10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine("]");
+}
+
+int SumOddPositionNumbers(int[] array)
+{
+    int sum = 0;
+    for(int i=1; i<array.Length; i+=2)
+       sum = sum + array[i];
+    return sum;
+}
